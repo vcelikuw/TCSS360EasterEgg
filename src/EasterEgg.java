eb852de99c295da90a4978f6d8967f3aa6867777
ecelikuw <ecelik@uw.edu>
2015-11-12 19:40:01 -0800

    add
    add project

---
+++ src/EasterEgg.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Emrullah
+ *
+ */
+public class EasterEgg {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Emrullah");
+
+	}
+
+}
